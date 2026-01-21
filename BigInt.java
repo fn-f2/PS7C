48a685627ca4b69440abc59e3975051dec63d42e
carter <89955530+fn-f2@users.noreply.github.com>
2026-01-20 21:16:55 -0800

    Add files via upload

---
+++ BigInt.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class BigInt
+{
+    
+    public static void main(String[] args)
+    {
+        
+    }
+    
+    public static int[] add(int[] a, int[] b)
+    {
+        for (int i = 0; i < a.length; i++)
+        {
+            
+        }
+    }
+}
---
+++ GoldbachConjAndSieveOfEratosthenes.java
@@ -0,0 +1,75 @@
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class GoldbachConjAndSieveOfEratosthenes
+{
+
+    public static void main(String[] args)
+    {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter an even number greater than 2");
+
+        if (!in.hasNextInt())
+        {
+            System.out.println("Not a valid number");
+            System.exit(0);
+        }
+
+        int num = in.nextInt();
+
+        if (num%2 == 1 || num == 2)
+        {
+            System.out.println("Not a valid number");
+            System.exit(0);
+        }
+
+        ArrayList<Integer> nums = sieve(num);
+
+        int ans1 = 0;
+        int ans2 = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = i; j < nums.size(); j++)
+            {
+                if (nums.get(i) + nums.get(j) == num)
+                {
+                    ans1 = nums.get(i);
+                    ans2 = nums.get(j);
+                }
+            }
+        }
+
+        System.out.println(num + " = " + ans1 + " + " + ans2);
+    }
+
+    public static ArrayList<Integer> sieve(int n)
+    {
+        ArrayList<Integer> lst = new ArrayList();
+
+
+        for (int i = 0; i < n-2; i++)
+        {
+            lst.add(i+2);
+        }
+        
+        int p = 2;
+        int i = 0;
+        while (p <= Math.sqrt(n))
+        {
+            for (int j = 0; j < lst.size(); j++)
+            {
+                if (p != lst.get(j) && lst.get(j)%p == 0)
+                {
+                    lst.remove(j);
+                    j--;
+                }
+            }
+            i++;
+            p = lst.get(i);
+        }
+
+        return lst;
+    }
+}
